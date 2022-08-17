9cb537074ce1de37957e91b7d8e32def813b8b3d
francoso77 <frank77invest@gmai.com>
2022-08-17 13:41:19 -0300

    atual 17-08

---
+++ alunos/src/interfaces/AlunoInterfaces.ts
@@ -0,0 +1,10 @@
+interface AlunoInterface
+{
+    matricula: string
+    nomeAluno: string
+    nota1?: number
+    nota2?: number
+    nota3?: number
+    notaFinal?: number
+    status?: string
+}
