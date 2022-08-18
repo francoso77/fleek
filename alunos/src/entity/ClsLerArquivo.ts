640743341a6ed3f94d8bb6775fc029f2cfdbc89e
francoso77 <frank77invest@gmai.com>
2022-08-18 14:29:22 -0300

    atual 14-29

--- alunos/src/entity/ClsLerArquivo.ts
+++ alunos/src/entity/ClsLerArquivo.ts
@@ -1,27 +1,29 @@
 import fs from 'node:fs/promises'
+import lineReader from 'line-reader'
+
 
 export default class ClsLeituraArquivo
 {
     constructor(private nomeArquivo: string){}
-    public conteudoArquivo: string = ''
-    
-    public lerArquivo(): Promise<string> {
 
-        return fs.readFile(this.nomeArquivo).then( (texto) =>
-        {
-            var linhas: Array<string> = texto.toString().split(' ')
-            
-            
-            //linhas.forEach(function (linha){
-            //    console.log(linha)
-           // })
-            console.log('linha 0', linhas)
-            this.conteudoArquivo = texto.toString()
-            return Promise.resolve(texto.toString())
-    
-        }).catch( err1 =>
-        {
-             return Promise.reject(err1.message)
+    public lerArquivo(): Promise<string [][]>{
+        
+        /*let dadosAlunos: string[][] = []
+        lineReader.eachLine(this.nomeArquivo, function(linha){
+            dadosAlunos.push(linha.split(" "))
         })
-    }
+        return fs.readFile */
+        let dadosAlunos: string[][] = []
+
+        return new Promise((resolve, reject) => {
+
+            lineReader.eachLine(this.nomeArquivo, function(linha){
+                dadosAlunos.push(linha.split(" "))
+            }).then(() => {
+                resolve(dadosAlunos)
+            }).catch (err => {
+                reject ('Erro na leitura')
+            })
+        })
+    }    
 }
---
+++ alunos/src/index_old.ts
@@ -0,0 +1,53 @@
+import ClsLeituraArquivo from "./entity/ClsLerArquivo";
+import { AlunoInterface } from "./interfaces/AlunoInterfaces";
+
+
+let dadosAlunos: Array<AlunoInterface> = [{
+    matricula: '801',
+    nomeAluno: 'Frank',
+    nota1: 5.7,
+    nota2: 6.2,
+    nota3: 9.0,
+    notaFinal: 0,
+    status: ''
+}, {
+    matricula: '001',
+    nomeAluno: 'Maria',
+    nota1: 1.7,
+    nota2: 10.0,
+    nota3: 8.5,
+    notaFinal: 0,
+    status: ''
+}]
+
+let clsLeitura: ClsLeituraArquivo = new ClsLeituraArquivo('alunos.txt')
+
+clsLeitura.lerArquivo().then(retornoArquivo =>
+{
+    console.log('o retorno da função leitura:\n', retornoArquivo)
+}).catch (err2 =>
+{
+    console.log('arquivo não encontrado', err2.message)
+})
+
+/*for (var i:number = 0; dadosAlunos.length > i; i++)
+{
+    dadosAlunos[i].notaFinal =  (dadosAlunos[i].nota1 + dadosAlunos[i].nota2 + dadosAlunos[i].nota3)/3
+    dadosAlunos[i].notaFinal >= 7? dadosAlunos[i].status = 'Aprovado':dadosAlunos[i].status = 'Reprovado'
+}
+console.log(dadosAlunos)
+
+GeraCsv(dadosAlunos)
+
+function GeraCsv(obj: Array<AlunoInterface>): void
+{
+    var csv: string = 'Matricula, Aluno, NotaFInal, Status\n'
+
+    obj.forEach((row) =>{
+        csv += row.matricula
+        csv += row.nomeAluno
+        csv += row.notaFinal
+        csv += row.status
+        csv += '\n'
+    })
+}*/
