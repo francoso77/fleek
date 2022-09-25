d7ccff4b878006961d2f3123ae403e23c627ed4b
Frank Alves <frank77invest@gmail.com>
2022-09-25 20:38:57 -0300

    commit inicial

--- lab_teste/src/components/frmCao.vue
+++ lab_teste/src/components/frmCao.vue
@@ -1,42 +1,70 @@
 <template>
     <div class="frmCao">
-        <div calss="titulo">
-            <h2>Perfil do cão</h2>
-        </div>
+
+        <h2>Perfil do cão</h2>
         <span>Qualquer alteração neste formulário irá afetar apenas eventos futuros</span>
         <label for="lblCodigo">Código</label>
-        <input type="number" id="txtCodigo" />
+        <input type="text" id="txtCodigo" v-model="rsCao.id" />
 
         <label for="lblNome">Nome (curto)</label>
-        <input type="text" id="txtNome" maxlength="35"/>
+        <input type="text" autofocus placeholder="nome" id="txtNome" maxlength="35" v-model="rsCao.nome" />
 
         <label for="lblGenero">Género</label>
-        <select>
+        <select v-model="rsCao.genero">
             <option disabled selected>Género</option>
             <option>Femea</option>
             <option>Macho</option>
         </select>
 
         <label for="lblAltura">Altura (cm)</label>
-        <input type="number" id="txtAltura" />
+        <input type="number" id="txtAltura" v-model="rsCao.altura" />
 
         <label for="lblNascimento">Data de Nascimento</label>
-        <input type="date" id="txtNascimento" />
+        <input type="date" id="txtNascimento" v-model="rsCao.nascimento" />
 
         <label for="lblObito">Data de óbito</label>
-        <input type="date" id="txtObito" />
+        <input type="date" id="txtObito" v-model="rsCao.obito" />
 
         <label for="lblRaca">Raça</label>
-        <select>
+        <select v-model="rsCao.raca">
             <option disabled selected>Raças</option>
             <option v-for="raca in racas" :value="raca">{{raca}}</option>
         </select>
 
         <label for="lblFederacao">Federações</label>
-        <select>
+        <select v-model="rsCao.federacao">
             <option disabled selected>Federações</option>
             <option v-for="federacao in federacoes" :value="federacao">{{federacao}}</option>
         </select>
+
+        <label for="lblCategoria">Categorias</label>
+        <div class="categoria">
+
+            <input type="radio" id="radCatBranca" value="Branca" v-model="rsCao.categoria" /><br>
+            <label for="radBranca">Branca</label>
+            <input type="radio" id="radCatAzul" value="Azul" v-model="rsCao.categoria" /><br>
+            <label for="radCatAzul">Azul</label>
+            <input type="radio" id="radCatAmarela" value="Amarela" v-model="rsCao.categoria" /><br>
+            <label for="radCatAmarela">Amarela</label>
+        </div>
+
+        <label for="lblSaude">Saúde</label>
+        <div class="saude">
+
+
+            <input type="checkbox" id="chkVacina" value="" true-value="sim" false-value="não" v-model="rsCao.vacina" />
+            <label for="chkVacina">Vacinação em dia?</label>
+            <input type="checkbox" id="chkConsulta" value="" true-value="sim" false-value="não"
+                v-model="rsCao.consulta" />
+            <label for="chkConsulta">Consultas em dia?</label>
+            <label for="txtClinica">Clínica Veterinária</label>
+            <input type="text" id="txtClinica" value="" maxlength="30" v-model="rsCao.clinica" />
+
+
+        </div>
+        <div class="debug">
+            {{rsCao}}
+        </div>
 
     </div>
 </template>
@@ -45,5 +73,4 @@
 import frmCaoCls from './frmCaoCls';
 export default frmCaoCls
 </script>
-
-<style src="./frmCaoCss.css"></style>
+<style scoped src="./frmCaoCss.css">
--- lab_teste/src/components/frmCaoCls.ts
+++ lab_teste/src/components/frmCaoCls.ts
@@ -1,8 +1,38 @@
 import { Component, Vue } from 'vue-property-decorator';
 
+interface CaoInterface{
+    id: string,
+    nome: string,
+    genero: string,
+    altura: number,
+    nascimento: string,
+    obito: string,
+    raca: number,
+    federacao: number,
+    categoria: string,
+    vacina: boolean,
+    consulta: boolean,
+    clinica: string
+}
 
 @Component
 export default class frmCaoCls extends Vue {
+
+    public rsCao: CaoInterface = {
+        id: '',
+        nome:'',
+        genero:'',
+        altura:0,
+        nascimento:'',
+        obito:'',
+        raca: 0,
+        federacao: 0,
+        categoria: '',
+        vacina: false,
+        consulta: false,
+        clinica: ''
+
+    }
 
     public racas: Array<string> = [
         'PitBull',
